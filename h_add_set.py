a23668bfc2b19ca35682de0cd445ff37237119b8
yym <583692365@qq.com>
2020-01-09 22:33:42 +0800

    集合删除和添加

---
+++ h_add_set.py
@@ -0,0 +1,35 @@
+# author yym
+# mset = set(["零基础学Java", "零基础学PHP", "零基础学CPP", "零基础学python"])
+# print(mset)
+# mset.add("高级水平Java")
+# print(mset)
+
+# 2
+# # remove()删除指定  pop()随机删除  clear()清空
+# mset.remove("零基础学Java")
+# print("remove", mset)
+# temp = mset.pop()
+# print("输出被删除的集合中的元素", temp)
+# print("pop", mset)
+# mset.clear()
+# print("clear", mset)
+
+# 3
+python = {"王一伊", "李静", "李明", "李白"}
+c = {"李静", "李明", "d杜甫", "杨过"}
+python.add(c.pop())  # 把杨过改成学python
+print(python)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
